c1d00396e6dbf9e61c2b1be87386bcbfaea3fd73
이창훈 <shlee807@naver.com>
2023-01-11 10:50:58 +0900

    conflict

--- src/main/java/com/example/tomato/vo/AddressVO.java
+++ src/main/java/com/example/tomato/vo/AddressVO.java
@@ -1,12 +1,23 @@
 package com.example.tomato.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+/*
+------------ -------- ------------ 
+NO           NOT NULL NUMBER       
+SIDO_NAME             VARCHAR2(60) 
+SIGUNGU_NAME          VARCHAR2(60) 
+DONG_NAME             VARCHAR2(60)
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class AddressVO {
-
-    private int no;
-    private String sidoName;
-    private String sigunguName;
-    private String dong_name;
+	private int no;
+	private String sidoName;
+	private String sigunguName;
+	private String dongName;
 }
